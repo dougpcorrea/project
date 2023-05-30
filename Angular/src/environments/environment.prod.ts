a3e7aa557ca3db235ba07d3a0abcac868dc1620f
Douglas <dougpcorrea@hotmail.com>
2023-05-30 00:10:01 -0300

    Enviroment updates

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-import { TasksComponent } from "src/app/tasks/tasks.component";
-
 export const environment = {
   production: true,
   apiUrl: 'https://lifeplannerapi.vercel.app/api/',
-  dashboard: TasksComponent,
+  localDbEnabled: false,
 };
