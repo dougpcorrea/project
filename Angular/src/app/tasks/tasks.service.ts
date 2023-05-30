312569cbbad97de287c1c0d864f0ec79ed74817e
Douglas <dougpcorrea@hotmail.com>
2023-05-29 22:33:39 -0300

    App enviroment test

--- Angular/src/app/tasks/tasks.service.ts
+++ Angular/src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
 
   getTasks() {
-    return this.httpClient.get<any>('http://localhost:8000/api/task')
+    return this.httpClient.get<any>(`${environment.apiUrl}task`)
   }
 
   addTask(id: number, task: string, date: any, priority: any, project: any,  ordering: number, status: number, subtask: number, repeat: number){
@@ -29,43 +30,43 @@ export class DataService {
     }
 
     const taskInterface: TaskInterface = {id, task, date, priority, project, ordering, status, subtask, repeat};
-    return this.httpClient.post('http://localhost:8000/api/task/', taskInterface).subscribe();
+    return this.httpClient.post(`${environment.apiUrl}task`, taskInterface).subscribe();
   }
 
   deleteTask(id: number){
-    this.httpClient.delete(`http://localhost:8000/api/task/${id}`).subscribe();
+    this.httpClient.delete(`${environment.apiUrl}task/${id}`).subscribe();
   }
 
   updateTaskName(id: number, task: string){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {task: task}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {task: task}).subscribe();
   }
 
   updateTaskStatus(id: number, status: number){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {status: status}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {status: status}).subscribe();
   }
 
   updateTaskDate(id: number, date: any){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {date: date}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {date: date}).subscribe();
   }
 
   updateRepeat(id: number, repeat: number){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {repeat: repeat}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {repeat: repeat}).subscribe();
   }
 
   updateProject(id: number, project: string){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {project: project}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {project: project}).subscribe();
   }
 
   updateTaskPriority(id: number, priority: number){
-    this.httpClient.put(`http://localhost:8000/api/task/${id}/`, {priority: priority}).subscribe();
+    this.httpClient.put(`${environment.apiUrl}task/${id}/`, {priority: priority}).subscribe();
   }
 
   getKarma(){
-    return this.httpClient.get<any>('http://localhost:8000/api/karma')
+    return this.httpClient.get<any>(`${environment.apiUrl}karma`)
   }
 
   updateKarma(date: string, completed: number){
-    this.httpClient.post('http://localhost:8000/api/karma/', {date: date, completed: completed}).subscribe();
+    this.httpClient.post(`${environment.apiUrl}karma`, {date: date, completed: completed}).subscribe();
   }
 
 
